921da6bc0582125c30bf47f2a649824ab31c74ab
Marcellus Major <marcellusmajor@agetechnology.com>
2018-03-07 00:25:31 -0500

    Added Entities

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,9 +1,17 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { ContractMonoAddressMySuffixModule } from './address-my-suffix/address-my-suffix.module';
+import { ContractMonoEmailMySuffixModule } from './email-my-suffix/email-my-suffix.module';
+import { ContractMonoPhoneMySuffixModule } from './phone-my-suffix/phone-my-suffix.module';
+import { ContractMonoContactMySuffixModule } from './contact-my-suffix/contact-my-suffix.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
+        ContractMonoAddressMySuffixModule,
+        ContractMonoEmailMySuffixModule,
+        ContractMonoPhoneMySuffixModule,
+        ContractMonoContactMySuffixModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
